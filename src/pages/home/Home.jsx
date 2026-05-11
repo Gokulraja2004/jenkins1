8573d185625fa65b88586258839226672cd5fe49
Gokulraja2004 <gg7537007@gmail.com>
2026-05-11 17:47:55 +0530

    Fixed case-sensitive imports

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import Header from '../../components/header/Header'
 import ExplorMenu from '../../components/ExplorMenu/ExplorMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../components/Appdownload/Appdownload'
+import AppDownload from '../../components/Appdownload/AppDownload'
 
 const Home = () => {
   const [category, setCategory]=useState('All')
