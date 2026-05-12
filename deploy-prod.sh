0e35d7da4e8e36d7cf7f9f74a7048f85efaab29a
Gokulraja2004 <gg7537007@gmail.com>
2026-05-12 19:15:53 +0530

    bxnz

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
 
+HOST=$1
+USER=$2
+PASS=$3
 
-sshpass -p "$PASS" scp -o StrictHostKeyChecking=no -r dist/* $USER@$HOST:/home/ubuntu/prod-app
+sshpass -p "$PASS" scp -o StrictHostKeyChecking=no react-app.tar $USER@$HOST:/home/ubuntu
 
 sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no $USER@$HOST "
-    sudo rm -rf /var/www/html/* &&
-    sudo cp -r /home/ubuntu/prod-app/* /var/www/html/ &&
-    sudo systemctl restart nginx
+
+    docker stop react-prod-container || true &&
+    docker rm react-prod-container || true &&
+
+    docker rmi react-app || true &&
+
+    docker load < /home/ubuntu/react-app.tar &&
+
+    docker run -d \
+    --name react-prod-container \
+    -p 3000:80 \
+    react-app
 "
